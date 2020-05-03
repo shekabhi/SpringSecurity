57b9b4dda7c907867d9a8f99a6ab9019aa1eff69
shekabhi369 <shekabhi369@gmail.com>
2020-05-03 17:50:17 +0530

    Initial commit

---
+++ src/main/java/io/javabrains/springsecurityjpa/HomeResource.java
@@ -0,0 +1,26 @@
+package io.javabrains.springsecurityjpa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping (value = "/")
+    public String home() {
+        return ("<h1>hybrid payment security model</h1>");
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return ("<h1>Welcome User</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return ("<h1>Welcome Admin</h1>");
+    }
+}
+
